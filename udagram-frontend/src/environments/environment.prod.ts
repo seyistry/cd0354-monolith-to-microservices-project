903313308addc86433eb507e8889455e6260ec96
seyistry <seyistry@gmail.com>
2022-07-12 12:49:36 +0100

    update frontend url

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab7b0b1f8e05047268974c16afbc9796-381413575.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
