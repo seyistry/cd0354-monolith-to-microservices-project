f14c441b09993fccbcc55b230c80d064f7188357
seyistry <seyistry@gmail.com>
2022-07-12 22:36:42 +0100

    update proxy

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://ab7b0b1f8e05047268974c16afbc9796-381413575.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'a1542df1a522244caa103af1f35ebbe9-671054167.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
